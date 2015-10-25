2f4219b5cffbcf7946e034d4b85598aa22350917
kellim <kelli.blalock2@gmail.com>
2015-10-25 10:41:09 -0700

    Display map and characters

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = 0;
+    this.y = 60;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -24,12 +25,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 0;
+    this.y = 400;
+    this.sprite = 'images/char-princess-girl.png';
+}
 
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var enemy1 = new Enemy();
+var allEnemies = [enemy1];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
